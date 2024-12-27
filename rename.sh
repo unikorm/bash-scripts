b7b0b82c7bbf8aff2a6d4bfffdc4f7b0c8a77a5a
adamko <adaled00@gmail.com>
2024-12-27 19:09:56 +0100

    feat(rename): add

---
+++ rename.sh
@@ -0,0 +1,8 @@
+count=5
+
+for file in *.jpg; do
+    [ -e "$file" ] || continue
+    new_name="image-${count}-portrait.jpg"  // change the new name as per your requirement
+    mv "$file" "$new_name"
+    ((count++))
+done
